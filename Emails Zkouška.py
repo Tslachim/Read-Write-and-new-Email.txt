5b0659e444b274921176dcf68acd2c6f773fc0c7
Michal Stuchlík <tslachim@gmail.com>
2023-01-18 22:17:31 +0100

    Cvičný projekt na čtení a psaní txt souboru

---
+++ Emails Zkouška.py
@@ -0,0 +1,11 @@
+# Načtení dopisu 
+with open("C:/Users/tslac/OneDrive/Plocha/Projekty/Read-Write-and-new-Email/general_letter.txt") as letter:
+    letter_content = letter.read()
+
+with open("C:/Users/tslac/OneDrive/Plocha/Projekty/Read-Write-and-new-Email/names.txt") as names:
+    list_of_names = names.readlines()
+    for one_name in list_of_names:
+        one_name = one_name.strip()
+        letter_text = letter_content.replace("[name]", one_name)
+        with open(f"output/letter_{one_name}.txt", mode= "w") as final_letter:
+            final_letter.write(letter_text)
